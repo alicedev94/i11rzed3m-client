2145f02a67e24518bd38cd4f28999918c7200bcb
alicedev94 <alicedev94@gmail.com>
2024-11-18 15:35:45 -0400

    sss

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import serviceWorker from './modules/service-worker/main.services.js';
 import { sum } from './modules/sum.js';
 
+
 export { sum, serviceWorker };
--- modules/service-worker/main.services.js
+++ modules/service-worker/main.services.js
@@ -1,9 +1,20 @@
+import webpush from "web-push";
+
 export default class serviceWorker {
-    static active () {
-        if("serviceWorker" in navigator) {
-            alert("serviceWorker: active");
-        } else {
-            alert("serviceWorker: false");
-        }
+  static active() {
+    if ("serviceWorker" in navigator) {
+      alert("serviceWorker: active");
+    } else {
+      alert("serviceWorker: false");
     }
+  }
+
+  init() {
+    webpush.setVapidDetails(mailto, publicKey, privateKey);
+  }
+
+  async sendNotification(subscription, payload) {
+    const response = await webpush.sendNotification(subscription, payload);
+    return response;
+  }
 }

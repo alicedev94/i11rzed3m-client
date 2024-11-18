800feb69766cc7de613df3c86832ff062b7d4bb6
alicedev94 <alicedev94@gmail.com>
2024-11-18 15:43:33 -0400

    Server service

--- modules/service-worker/main.services.js
+++ modules/service-worker/main.services.js
@@ -1,6 +1,7 @@
 import webpush from "web-push";
 
 export default class serviceWorker {
+  // Client service
   static active() {
     if ("serviceWorker" in navigator) {
       alert("serviceWorker: active");
@@ -9,10 +10,12 @@ export default class serviceWorker {
     }
   }
 
+  // Server service
   init() {
     webpush.setVapidDetails(mailto, publicKey, privateKey);
   }
 
+  // Server service
   async sendNotification(subscription, payload) {
     const response = await webpush.sendNotification(subscription, payload);
     return response;

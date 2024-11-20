cee0fe9dc9fc1a725f3c6889be5c57ca7d90076a
alicedev94 <alicedev94@gmail.com>
2024-11-20 09:43:07 -0400

    s

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // import serviceWorker from './modules/service-worker/main.services.js';
 // import { configurePushNotifications } from './modules/service-worker/sw.config.js';
 import { sum } from './modules/sum.js';
+import { active } from './modules/active.js';
 
 // export default serviceWorker;
 // export { configurePushNotifications };
-export { sum };
+export { sum, active };

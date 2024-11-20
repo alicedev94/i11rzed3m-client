ed83fccc8bebc5f3370e1c2b0784c536a8ec77bf
alicedev94 <alicedev94@gmail.com>
2024-11-20 16:15:44 -0400

    s

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// use client
+// Use client
 export { active, urlBase64ToUint8Array } from './modules/push/client.js'
+
+// Use server
 // export { init, sendNotification } from './modules/push/server.js'
 
 

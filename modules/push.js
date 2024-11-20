42cb613669f73413f25fdeda44f134e806751152
alicedev94 <alicedev94@gmail.com>
2024-11-20 11:10:56 -0400

    s

--- modules/push.js
+++ modules/push.js
@@ -10,16 +10,16 @@ export function active() {
 }
 
 // Client service
-// export const urlBase64ToUint8Array = (base64String) => {
-//   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-//   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-//   const rawData = window.atob(base64);
-//   const outputArray = new Uint8Array(rawData.length);
-//   for (let i = 0; i < rawData.length; ++i) {
-//     outputArray[i] = rawData.charCodeAt(i);
-//   }
-//   return outputArray;
-// };
+export const urlBase64ToUint8Array = (base64String) => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
 
 // // Server service
 // export const init = (publicKey, privateKey) => {

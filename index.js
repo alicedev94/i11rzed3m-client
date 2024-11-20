94eb7d00f5a58a5317fe3b868bdd1a01c56810a8
alicedev94 <alicedev94@gmail.com>
2024-11-20 11:00:30 -0400

    this version is funtional with new sintaxis

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // import { active, urlBase64ToUint8Array, init, sendNotification } from './modules/push.js';
 
-// export { active, urlBase64ToUint8Array, init, sendNotification };
-
-// import { active } from './modules/push.js';
-
-// export { active };
-
 export { sum, active } from './modules/push.js'
 
 
